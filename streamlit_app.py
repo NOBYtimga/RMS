238ed58f80045aeeb6d9e2eed84469ea0d15e331
NOBYtimga <tgaleev18011982@gmail.com>
2024-08-30 12:11:09 +0300

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,33 +3,24 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-st.header('st.write')
 
-# Example 1
+st.header('Система риск-менеджмента в ЭТП ГПБ')
 
-st.write('Hello, *World!* :sunglasses:')
 
-# Example 2
 
-st.write(1234)
 
-# Example 3
+with st.form("my_form"):
+   bp = st.text_input('Описание бизнес-процесса')
+   proc_importance = st.number_input('Критичность процесса',min_value=1,max_value=4,step=1,placeholder='Введите значение от 1 до 4',value=None)
+   submitted = st.form_submit_button("Отправить")
+   if submitted:
+     st.write(bp)
+     st.write(proc_importance)
 
-df = pd.DataFrame({
-     'first column': [1, 2, 3, 4],
-     'second column': [10, 20, 30, 40]
-     })
-st.write(df)
 
-# Example 4
 
-st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
 
-# Example 5
 
-df2 = pd.DataFrame(
-     np.random.randn(200, 3),
-     columns=['a', 'b', 'c'])
-c = alt.Chart(df2).mark_circle().encode(
-     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-st.write(c) 
+
+
+
