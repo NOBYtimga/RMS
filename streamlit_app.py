c5efaf2fa31a563d96d7048a57743d3f71b2d69e
NOBYtimga <tgaleev18011982@gmail.com>
2024-09-11 11:59:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.header('Система риск-менеджмента в ЭТП ГПБ')
 
 
 with st.form("my_form"):
-   bp = st.text_input('Описание бизнес-процесса')
+   bp = st.text_input('Описание бизнес-процесса' Ура!)
    proc_importance = st.number_input('Критичность процесса',min_value=1,max_value=4,step=1,placeholder='Введите значение от 1 до 4',value=None)
    submitted = st.form_submit_button("Отправить")
    if submitted:
